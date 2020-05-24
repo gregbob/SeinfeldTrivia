288da3fb2cb57bd75a7b65f610bc514195eee25d
Greg Rubenstein <gregoryrubenstein@gmail.com>
2020-05-23 22:43:33 -0400

    cleanup frontend

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
 name: 'App',
-  components: {
-    HelloWorld
-  },
   mounted() {
     this.$http.get('http://192.168.0.197:3000/test').then((response) => {
       console.log(response.data);
@@ -23,20 +17,9 @@ name: 'App',
     connect: function () {
       console.log('socket connected!');
     },
-    custom: function () {
-      console.log('custom emit');
-    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
