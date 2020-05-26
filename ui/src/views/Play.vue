ee7371b5c3d659cab57070f023a07c15659907c7
Greg Rubenstein <gregoryrubenstein@gmail.com>
2020-05-26 11:17:49 -0400

    play state change

--- ui/src/views/Play.vue
+++ ui/src/views/Play.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="play">
-    <input v-model="inputValue">
-    <button v-on:click="pushItem"> SUBMIT </button>
+    <div v-if="currentGameState == GameState.SETUP">
+      Waiting for game to start.
+    </div>
+    <div v-else-if="currentGameState == GameState.QUESTION">
+      <input v-model="inputValue" :disabled="submitted">
+      <button v-on:click="submitAnswer"> SUBMIT </button>
+    </div>
+    <div v-else-if="currentGameState == GameState.ANSWER">
+      Waiting for host to decide the valid answers.
+    </div>
+
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import GameState from '../utils/gameState';
 
 export default {
   name: 'Play',
   data() {
     return {
-      inputValue: ''
+      inputValue: '',
+      submitted: false,
+      GameState
     }
   },
+  computed: {
+    ...mapGetters(['currentGameState'])
+  },
   methods: {
-    pushItem() {
+    submitAnswer() {
       const payload = { answer: this.inputValue }
       this.$socket.emit('submitAnswer', payload, (response) => {
         // If response is true, disable submit box and wait for state update
         console.log(response);
+        this.inputValue = '';
+        this.submitted = true;
       });
-
-      this.inputValue = '';
     }
-  },
+  }
 }
 </script>
 
