54b20ae2c0160bd6ce71e2a8f1d0b492f7dada6c
Greg Rubenstein <gregoryrubenstein@gmail.com>
2020-05-27 15:28:19 -0400

    add bootstrap

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,12 +4,17 @@ import VueAxios from 'vue-axios'
 import VueSocketIO from 'vue-socket.io'
 import SocketIO from "socket.io-client"
 
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
 Vue.use(new VueSocketIO({
   debug: true,
   connection: SocketIO('http://192.168.0.224:3000'),
