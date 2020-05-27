4c17de69273a573be75399c784963e38bfdcde04
Greg Rubenstein <gregoryrubenstein@gmail.com>
2020-05-27 16:43:33 -0400

    navbar

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <b-navbar type="dark" variant="primary">
+      <b-navbar-brand to="/">Seinfeld Trivia</b-navbar-brand>
+    </b-navbar>
     <router-view></router-view>
   </div>
 </template>
