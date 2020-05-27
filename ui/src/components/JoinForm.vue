a2a590d996c3faa22f91bdb9502831d0d0fb729d
Greg Rubenstein <gregoryrubenstein@gmail.com>
2020-05-27 16:42:10 -0400

    update ui for join form

---
+++ ui/src/components/JoinForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="join-form">
+    <b-form @submit="onSubmit">      
+      <b-form-group label="Your Name">
+        <b-form-input v-model="name" required placeholder="Enter name"></b-form-input>
+      </b-form-group>
+      <b-form-group label="Room Code">
+        <b-form-input v-model="roomCode" required placeholder="Enter room code"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary"> Join Room </b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JoinForm',
+  data() {
+    return {
+      name: '',
+      roomCode: ''
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      const payload = {
+        roomCode: this.roomCode,
+        user: {
+          name: this.name
+        }
+      };
+      this.$socket.emit('addUser', payload, (response) => {
+        if (response == true) {
+          this.$router.push(`/play`);
+        } else {
+          this.$bvToast.toast(`Failed to join room: ${this.roomCode}`, {
+            title: 'Error',
+            solid: true,
+            variant: "danger",
+            toaster: 'b-toaster-top-full',
+          });
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#join-form {
+  margin: 1em;
+}
+</style>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,47 +1,28 @@
 <template>
   <div id="home">
-    <router-link to="/host">Go to Host</router-link>
-    <div id="join-info">
-      <label> Name </label>
-      <input id="user-name" v-model="userName">
-      <label> Room Code </label>
-      <input id="room-code" v-model="roomCode">
-      <button v-on:click="addUser(userName, roomCode)"> Join Room </button>
-      <!-- <router-link to="/play">Join</router-link> -->
-    </div>
+    <b-tabs content-class="mt-3">
+      <b-tab title="Host">
+        <router-link to="/host">Go to Host</router-link>
+      </b-tab>
+      <b-tab title="Join">
+        <join-form></join-form>
+      </b-tab>
+    </b-tabs>
+
+
   </div>
 </template>
 
 <script>
+import JoinForm from '../components/JoinForm';
+
 export default {
   name: 'Home',
-  data() {
-    return {
-      userName: '',
-      roomCode: ''
-    };
-  },
-  methods: {
-    addUser() {
-      const payload = {
-        roomCode: this.roomCode,
-        user: {
-          name: this.userName
-        }
-      };
-      this.$socket.emit('addUser', payload, (response) => {
-        if (response == true) {
-          this.$router.push(`/play`);
-        }
-      });
-    }
+  components: {
+    JoinForm,
   }
 }
 </script>
 
 <style>
-#join-info {
-  display: flex;
-  flex-direction: column;
-}
 </style>
